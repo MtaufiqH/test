4f6b3ea65bf0371cb5bf169445edd35290baecef
mtaufiqh62@gmail.com <123darussalam>
2019-05-13 13:44:43 +0800

    Does App

---
+++ app/src/main/java/com/example/taufiq/doesapp/MyDoes.java
@@ -0,0 +1,31 @@
+package com.example.taufiq.doesapp;
+
+public class MyDoes {
+
+    private String title,desc,date;
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
